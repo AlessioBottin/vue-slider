b4bccc9fbab7ee0318e2cecc102bb4fd2f00ecd0
AlessioBottin <alessiobottin2002@gmail.com>
2021-12-03 14:55:10 +0100

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,38 @@
+Vue.config.devtools = true;
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        currentActiveSlideIndex: 0,
+        slides: [
+            {
+                imgSrc: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                imgSrc: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum',
+            },
+            {
+                imgSrc: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                imgSrc: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                imgSrc: 'img/05.jpg',
+                title: 'Svizzera',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            }
+        ]
+    },
+    methods: {
+
+    }
+})
